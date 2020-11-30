59ebe95143adfebce4e4634a0020f812fc0a835c
Taylor <tjaroguetech@gmail.com>
2020-11-30 15:27:22 -0500

    commit tree 6d38a56d7ccf09c0d7e73b75230b16430aaf043f

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase(0) + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let firstWord = string.split(' ').slice(0, 1)
+    let otherWords = string.split(' ').slice('1')
+    let titleArr = []
+    
+    
+    titleArr.push(this.capitalize(firstWord[0]))
+    for(const word of otherWords) {
+      if(!exceptions.includes(word)){
+        titleArr.push(this.capitalize(word))
+      }
+      else {
+        titleArr.push(word)
+      }
+    }
+    return titleArr.join(' ')
+  }
+
 }
